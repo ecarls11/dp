c6623b066100557c6cef0b827ece185628a128ee
Annapurna Vadaparty <apvadaparty@gmail.com>
2018-05-04 15:55:05 -0400

    plot update

--- plot.py
+++ plot.py
@@ -11,8 +11,8 @@ with open('logtest.csv','r') as csvfile:
         train_accs.append(int(row[0]))
         train_losses.append(int(row[1]))
 
-plt.plot(train_accs, 'r')
-plt.plot(train_losses, 'b')
+acc, = plt.plot(train_accs, 'r', label = 'Training Accuracy')
+loss, = plt.plot(train_losses, 'b', label = 'Training Loss')
 print("Max train accuracy: ", max(train_accs))
 print("Max validation accuracy: ", max(train_losses))
 print("Training accuracy is red and training loss is blue. ")
@@ -21,5 +21,8 @@ print("Training accuracy is red and training loss is blue. ")
 plt.xlabel('Steps (100s)')
 plt.ylabel('Accuracy, Loss')
 plt.title('Training Accuracy and Loss')
+
+plt.legend(handles=[acc, loss])
+
 plt.legend()
 plt.show()
