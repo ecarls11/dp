09d000700158dfe5822398ec18703e38c75f7de2
madboy102 <alexb826@gmail.com>
2018-05-12 13:15:28 -0400

    trying different combos of lr and optimizers

---
+++ run_vgg.sh
@@ -0,0 +1,11 @@
+python test_main.py --model vgg --lr 1e-2 --optimizer adam --save_model
+python test_main.py --model vgg --lr 1e-3 --optimizer adam --save_model
+python test_main.py --model vgg --lr 1e-4 --optimizer adam --save_model
+
+python test_main.py --model vgg --lr 1e-2 --optimizer sgd --save_model
+python test_main.py --model vgg --lr 1e-3 --optimizer sgd --save_model
+python test_main.py --model vgg --lr 1e-4 --optimizer sgd --save_model
+
+python test_main.py --model vgg --lr 1e-2 --optimizer rmsprop --save_model
+python test_main.py --model vgg --lr 1e-3 --optimizer rmsprop --save_model
+python test_main.py --model vgg --lr 1e-4 --optimizer rmsprop --save_model
