949498789eb81a7ee0737590b008dd8d5c8954b0
madboy102 <alexb826@gmail.com>
2018-05-14 21:10:55 -0400

    plots

--- plot.py
+++ plot.py
@@ -2,55 +2,74 @@ import matplotlib.pyplot as plt
 import csv
 import numpy as np
 
-train_accs = np.zeros(1)
-train_losses = np.zeros(1)
-print(train_accs.shape)
 
-with open('../logs/val_vgg.csv','r') as f:
-    count = -1
-    for line in f:
-        # count += 1
-        # if count % 10 == 0:
+learn = ['0.01', '0.001', '0.0001']
+for lr in learn:
+    save_name = 'vgg_adam' + lr
+    train_accs = np.zeros(1)
+    train_losses = np.zeros(1)
+    val_accs = np.zeros(1)
+    val_losses = np.zeros(1)
+
+    with open('../logs/' + save_name + '/train.csv','r') as f:
+        for line in f:
             acc, loss = line.split()
             acc = float(acc)
             loss = float(loss)
             train_accs = np.append(train_accs, acc)
             train_losses = np.append(train_losses, loss)
-        # train_accs.append(acc)
-        # train_losses.append(loss)
 
-    train_accs = train_accs[1:]
-    train_losses = train_losses[1:]
+        train_accs = train_accs[1:]
+        train_losses = train_losses[1:]
 
-    steps = list(range(len(train_accs)))
-    for i in range(len(steps)):
-        steps[i] *= 1#100
-    steps = np.asarray(steps)
+        steps = list(range(len(train_accs)))
+        for i in range(len(steps)):
+            steps[i] *= 500
+        steps = np.asarray(steps)
 
-    # next(csvfile)
-    # rows = csv.reader(csvfile, delimiter=',')
-    # for row in rows:
-    #     print(row)
-    #     train_accs.append(float(row[0][0]))
-    #     train_losses.append(float(row[0][1]))
 
-plt.figure(1)
-plt.plot(steps, train_accs, 'r', label = 'Training Accuracy')
-plt.xlabel('Epoch')
-plt.ylabel('Accuracy')
-plt.title('Mini-VGG Validation Accuracy')
-# plt.legend(handles=[acc])
+    with open('../logs/' + save_name + '/val.csv','r') as f:
+        count = -1
+        for line in f:
+            acc, loss = line.split()
+            acc = float(acc)
+            loss = float(loss)
+            val_accs = np.append(val_accs, acc)
+            val_losses = np.append(val_losses, loss)
 
-plt.figure(2)
-plt.plot(steps, train_losses, 'b', label = 'Training Loss')
-plt.xlabel('Epoch')
-plt.ylabel('Loss')
-plt.title('Mini-VGG Validation Loss')
-# plt.legend(handles=[loss])
+        val_accs = val_accs[1:]
+        val_losses = val_losses[1:]
 
-print("Max train accuracy: ", max(train_accs))
-print("Min train loss: ", min(train_losses))
-print("Training accuracy is red and training loss is blue. ")
+        steps = list(range(len(val_accs)))
+        for i in range(len(steps)):
+            steps[i] *= 500
+        steps = np.asarray(steps)
+
+    plt.figure(1)
+    plt.plot(steps, train_accs, 'r', label='Training Accuracy')
+    plt.plot(steps, val_accs, 'b', label='Validation Accuracy')
+    plt.xlabel('Step')
+    plt.ylabel('Accuracy')
+    plt.title(save_name + '_acc.png')
+    plt.legend()
+    plt.savefig('pics/combined/' + save_name + '_acc.png')
+    plt.close(1)
+
+    plt.figure(2)
+    plt.plot(steps, train_losses, 'r', label='Training Loss')
+    plt.plot(steps, val_losses, 'b', label='Validation Loss')
+    plt.xlabel('Step')
+    plt.ylabel('Loss')
+    plt.title(save_name + '_loss.png')
+    plt.legend()
+    plt.savefig('pics/combined/' + save_name + '_loss.png')
+    plt.close(2)
+
+    print("Max train accuracy: ", max(train_accs))
+    print("Min train loss: ", min(train_losses))
+    print("Training accuracy is red and training loss is blue. ")
+
+    
 
 #plt.plot(x,y, label='Loaded from file!')
 
@@ -58,4 +77,4 @@ print("Training accuracy is red and training loss is blue. ")
 # plt.legend(handles=[acc, loss])
 
 # plt.legend()
-plt.show()
+# plt.show()
